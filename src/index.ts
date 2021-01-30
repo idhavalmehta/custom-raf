be94233bdf70b82fd2e74393b62abd8a3b74493c
Dhaval Mehta <idhavalmehta@gmail.com>
2021-01-30 22:27:21 +0530

    update demo

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,18 @@
 import CustomRAF from "./CustomRAF";
 const animationProvider = new CustomRAF(60);
 
-let handle: number;
-let lastTime: number;
+const element = document.getElementById("square");
+let start: number;
 
-let left = 0;
-const square = document.getElementById("square");
-const maxLeft =
-  document.body.getBoundingClientRect().width -
-  square.getBoundingClientRect().width;
+function step(timestamp: number) {
+  if (start === undefined) start = timestamp;
+  const elapsed = timestamp - start;
 
-function step(currentTime?: number) {
-  console.group("step");
-  console.log("timestamp", currentTime);
+  element.style.transform =
+    "translateX(" + Math.min(0.1 * elapsed, 200) + "px)";
 
-  const diff = currentTime - lastTime;
-  console.log("frameRate", diff);
-  lastTime = currentTime;
-
-  left = Math.min(left + diff, maxLeft);
-  console.log("left", left);
-  square.style.transform = "translateX(" + left + "px)";
-
-  if (left >= maxLeft) {
-    left = 0;
+  if (elapsed < 2000) {
+    animationProvider.requestAnimationFrame(step);
   }
-  handle = animationProvider.requestAnimationFrame(step);
-
-  console.groupEnd();
 }
-
-let isPlaying = false;
-const button = document.getElementById("button");
-
-button.onclick = () => {
-  if (isPlaying) {
-    console.log("cancelling animation");
-    animationProvider.cancelAnimationFrame(handle);
-  } else {
-    console.log("starting animation");
-    lastTime = performance.now();
-    handle = animationProvider.requestAnimationFrame(step);
-  }
-  isPlaying = !isPlaying;
-};
+animationProvider.requestAnimationFrame(step);
